97896e210a042ea1134272f129223704e74d5de1
charles <charles.marchildon2@gmail.com>
2021-12-10 21:30:50 -0500

    init

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import random
+import os
+
+### global variables
+minBet = 10
+startingMoney = 200
+###
+
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+def pause():
+  input("Press enter to continue...")
+
+rules = "insert rules"
+
+roundOver = False
+gameOver = False
+
 def createDeck():
     class Card:
         def __init__(self, int, suit):
@@ -28,45 +45,154 @@ def createDeck():
     
     return deck
 
-def rules():
-    print ("insert rules:")
+def menu():
+  print ("-h for the rules\n")
+  if bet != '':
+    print ("/n  current bet: {}".format(bet))
+  if players != []:
+    for data in players:
+      if roundOver:
+        print (" -{} {}$ {}/21".format(data.name, data.money, data.int))
+      else:
+        print (" -{} {}$".format(data.name, data.money))
+  
 
-def askInput(str):
+
+def askInput(str, msg="", displayRules=False):
+    clearConsole()
+    if displayRules: 
+      print (rules)
+      input ("\n\nPress enter to leave")
+      clearConsole()
+    
+    menu()
+
+    print ("\n")
+    if msg != '':
+      print (msg)
+
     inp = input(str)
-    if inp == "help":
-        rules()
-        askInput()
+    if inp.lower() == "--help" or inp.lower() == "-h":
+        askInput(str, "", True)
     else:
         return inp
 
 def newPlayer():
     class Player:
-        def __init__ (name, money):
+        def __init__ (self, name, money):
             self.name = name
             self.money = money
+            self.alive = True
+            self.win = False
+            self.int = 0
     name = askInput("name: ")
-    return Player(name, 120)
+    players.append(Player(name, startingMoney))
+    restart = askInput("Add a player? (y/n): ")
+    #restart = restart.lower() ?Doesn't work for some reason?
+    if restart == "y":
+      newPlayer()
 
-# def newRound(players):
-#         for player of players:
-#             print(player.name, player.money)
-#         def askBet(players):
-#             bet = askInput("Decide on a bet (>10$): ")
-#             if
+def newRound():
+        global roundOver
+        global gameOver
+        roundOver = False
+  
+        deck = createDeck()
+
+        def askBet(error=''):
+            bet = int(askInput("Decide on a bet (>{}$): ".format(minBet),error))
+            if (bet < minBet):
+                error = "bet too small"
+                askBet(error)
+            for data in players:
+                if data.alive:
+                  if data.money < bet:
+                    error = "{} doesn't have enough money".format(data.name)
+                    askBet(error)
+            else:
+              return bet
+            
+        bet = askBet()
+
+        for player in players:
+          player.money -= bet
+          player.int = 0
         
+        for player in players:
+          def subRound(player):
+            card = random.choice(deck)
+            if isinstance(card.int, int):
+              player.int += card.int
+            else:
+              player.int += 10
+
+            if player.int > 21:
+              print ("busted :(")
+              pause()
+            elif player.int == 21:
+              print ("blackjack!")
+              pause()
+            elif askInput("Stand or hit? (s/h): ", "{}'s turn with a score of {}".format(player.name, player.int)) == 'h':
+              subRound(player)
+
+          subRound(player)
+        
+        roundOver = True
+        alive = 0
+        max = 0
+        winnerCount = 0
+        for data in players:
+          if data.int <= 21 and data.int > max:
+            max = data.int
+        
+        if max != 0:
+          for data in players:
+            if data.int == max:
+              winnerCount += 1
+              data.win = True
+            else:
+              data.win = False
+          
+          winAmnt = bet * len(players) / winnerCount
+
+          for data in players:
+            if data.win:
+              data.money += winAmnt 
+
+        for player in players:
+            if player.money <= 0:
+              player.alive = False
+            else:
+              alive += 1
+              print(player.name, player.money)
+        if alive <= 1:
+          gameOver = True
+          endGame()
+          if askInput("Play again? (y/n): ", "foo won!!") == "y":
+            startGame
+
+        else:
+          askInput("Press enter to continue to next round...","foo won the round")
+          newRound()
+          
+
+    
+        
+
+def endGame():
+  print("foo won!!")   
+                    
 
 
 
 def startGame():
+    global players
+    global bet
+    global deck
+    bet = ''
     players = []
-    rules()
-    deck = createDeck()
-    players.append(newPlayer())
-    askPlayer = askInput("Add a player? (Y/n)")
-    if askPlayer.lower() == "y":
-        players.append(newPlayer())
+    newPlayer()
+    newRound()
 
+startGame()
 
-    
-
-    
